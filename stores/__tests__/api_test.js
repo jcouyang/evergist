0494d4e3aee254523d8766d02e88f19cfd3b0b1b
Jichao Ouyang <oyanglulu@gmail.com>
2014-12-21 23:33:02 +0800

    fix build

--- stores/__tests__/api_test.js
+++ stores/__tests__/api_test.js
@@ -6,9 +6,9 @@ describe('dispather', function(){
   var client = require('../client');
   it('return', function(){
     var gist = api('gists')('gistid')();
-    expect(client).toBeCalledWith({url:'/gists/gistid',method:'GET'});
+    expect(client).toBeCalledWith({path:'/gists/gistid',method:'GET'});
     gist = api('users')('jcouyang')('gists')();
-    expect(client).toBeCalledWith({url:'/users/jcouyang/gists',method:'GET'});
+    expect(client).toBeCalledWith({path:'/users/jcouyang/gists',method:'GET'});
   });
 });
 
