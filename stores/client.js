0c112c8a1fcd9b20dc0990ce50f8f62836f79fd4
Jichao Ouyang <oyanglulu@gmail.com>
2014-12-26 22:56:33 +0800

    add token for each request

--- stores/client.js
+++ stores/client.js
@@ -1,16 +1,15 @@
 var rest = require('rest'),
     pathPrefix = require('rest/interceptor/pathPrefix'),
     errorCode = require('rest/interceptor/errorCode'),
-    mime = require('rest/interceptor/mime');
-
+    mime = require('rest/interceptor/mime'),
+    defaultRequest = require('rest/interceptor/defaultRequest')
 var api_remote = 'http://private-4d99c-igist.apiary-mock.com';
 if (process.env.NODE_ENV === "prod") api_remote = 'https://api.github.com';
 client =
   rest.wrap(mime)
   .wrap(errorCode, { code: 500 })
-  .wrap(pathPrefix, { prefix: api_remote });
-//TODO token header
-// .wrap(defaultRequest, { headers: { 'token': 'rest.js' } });
+  .wrap(pathPrefix, { prefix: api_remote })
+  .wrap(defaultRequest, { headers: { 'Authorization': 'token '+ sessionStorage.access_token } });
 module.exports = client
 
 
