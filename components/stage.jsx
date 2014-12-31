95939a620b19af398cfa1d3b52702cf811aa22a6
Jichao Ouyang <oyanglulu@gmail.com>
2014-12-31 16:44:37 +0800

    logo and search bar

--- components/stage.jsx
+++ components/stage.jsx
@@ -1,10 +1,20 @@
-var GistList = require('./gist-list')
-
+var GistList = require('./gist-list'),
+React = require('react'),
+{AppBar, AppCanvas, Input} = require('material-ui');
 var Stage = React.createClass({
   render: function(){
     return (
       <div className="stage">
-        <GistList/>
+         <AppCanvas predefinedLayout={1}>
+           <AppBar className="mui-dark-theme" zDepth={0} title="!gist">
+             <div className="search-toolbar">
+               <input type='search'/>
+             </div>
+           </AppBar>
+           <div className="mui-app-content-canvas">
+             <GistList/>
+           </div>
+         </AppCanvas>
       </div>      
     )
   }
