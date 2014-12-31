ad82412650f9264dd209e83e2cbca8aaccd33477
Jichao Ouyang <oyanglulu@gmail.com>
2014-12-31 16:44:46 +0800

    gist card test

--- components/__tests__/gist-card-test.js
+++ components/__tests__/gist-card-test.js
@@ -1,21 +1,33 @@
 jest.dontMock('../gist-card');
-jest.setMock('../gist-detail', jest.genMockFunction())
+jest.setMock('../gist-detail', jest.genMockFunction());
 describe('gist card', function(){
-	it.only('render to dom with title from gist description', function(){
-		var React = require('react/addons');
-		var im = require('immutable')
-    var Paper = require('material-ui').Paper
-    var GistCard = require('../gist-card');
-  	var TestUtils = React.addons.TestUtils;
+  var React = require('react/addons');
+	var im = require('immutable');
+  var Paper = require('material-ui').Paper;
+  var GistCard = require('../gist-card');
+  var TestUtils = React.addons.TestUtils;
+
+	it('render to dom with title from gist description', function(){
   	var gist = im.fromJS({
-  		description: "iam title"
-  	})
+  		description: "iam a title"
+  	});
   	var gistCard = TestUtils.renderIntoDocument(
       <GistCard scrollTop={20} gist={gist} selected={false} setSelect={function(){}}/>
-  	)
-    console.log(gistCard)
-  	var paper = TestUtils.findRenderedDOMComponentWithClass(gistCard, 'gist-detail');
-    expect(paper.getDOMNode().textContent).toEqual('zdepth');
+  	);
+  	var title = TestUtils.findRenderedDOMComponentWithTag(gistCard, 'h3');
+    expect(title.getDOMNode().textContent).toEqual('iam a title');
+	});
 
-	})
-})
+  describe('when i hover on',function(){
+    it('zdepth should be 2', function(){
+  	  var gist = im.fromJS({
+  		  description: "iam a title"
+  	  });
+  	  var gistCard = TestUtils.renderIntoDocument(
+          <GistCard scrollTop={20} gist={gist} selected={false} setSelect={function(){}}/>
+  	  );
+  	  var paper = TestUtils.findRenderedDOMComponentWithClass(gistCard, 'mui-paper');
+      expect(paper.getDOMNode().textContent).toEqual('iam a title');
+	  });
+  });
+});
