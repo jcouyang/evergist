de202986149aecc76fdcda5ed1de79c2e146dbe5
Jichao Ouyang <oyanglulu@gmail.com>
2015-01-13 11:18:05 +0800

    update app url

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ app.on('ready', function() {
   mainWindow = new BrowserWindow({width: 800, height: 600});
 
   // and load the index.html of the app.
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.loadUrl('https://igist.io');
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
