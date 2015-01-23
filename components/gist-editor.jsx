fc863bd41d2ace7d8be125f66c2ae43503827ef8
Jichao Ouyang <oyanglulu@gmail.com>
2015-01-24 00:36:51 +0800

    snackbar

--- components/gist-editor.jsx
+++ components/gist-editor.jsx
@@ -1,7 +1,7 @@
 var React = require('react'),
 im = require('immutable'),
 Map= im.Map,
-{Paper, Toolbar, IconButton} = require('material-ui'),
+{Snackbar, Paper, Toolbar, IconButton, ToolbarGroup} = require('material-ui'),
 CodeMirrorEditor = require('./codemirror'),
 when = require('when');
 var Map = im.Map;
@@ -12,8 +12,7 @@ var loadingState = {
 	gists: [],
   editedGist: Map(),
   preview: false,
-  markdowns: im.List(),
-  languages: im.List()
+  markdowns: im.List()
 };
 CodeMirror.modeURL = "javascripts/vendor/codemirror/mode/%N/%N.js"
 
@@ -32,6 +31,8 @@ var GistEditor = React.createClass({
     this.setState({languages:languages})
   },
   render: function(){
+    if(!this.props.display)
+      return <div></div>
     var editors;
     if(this.state.preview){
       editors = this.state.markdowns.map((markdown)=>{
@@ -47,7 +48,8 @@ var GistEditor = React.createClass({
                             mode={file.get('language')}
                             onChange={this._handleChange}
                             filename={file.get('filename')}
-                            forceUpdate={true}/>)
+                            forceUpdate={true}
+                            lineWrapping={true}/>)
       }).toArray();
     }
     return (
@@ -56,13 +58,18 @@ var GistEditor = React.createClass({
           <img width="100%" src="assets/loading.svg"/>
         </div>
         <Toolbar>
-          <IconButton icon={this.state.preview?'image-edit':'image-remove-red-eye'} onClick={this._togglePreview}
-                      className={this.state.languages.contains('markdown')?'':'hidden'}/>
+          <ToolbarGroup float="right" key={1}>
+            <IconButton icon={this.state.preview?'image-edit':'image-remove-red-eye'}
+                        onClick={this._togglePreview}
+                        className={this.state.languages.contains('markdown')?'':'hidden'}/>
           <IconButton icon='content-save' onClick={this._saveGist} className={this.state.preview?'hidden':''}/>
+          </ToolbarGroup>
         </Toolbar>
         <div id={'editor-'+this.props.gistId}>
           {editors}
         </div>
+        <Snackbar ref="snackbar" message="saved!"/>
+        <Snackbar ref="save_fail" message="problem when saving! try again"/>
       </div>
     )
   },
@@ -71,8 +78,6 @@ var GistEditor = React.createClass({
       this.setState(loadingState)
       this._fetchGist(this.props.gistId)
     }
-  },
-  componentDidUpdate: function(){
   },
   _handleChange: function(filename, value){
     this.editedGist = this.props.files.setIn([filename,'content'], value)
@@ -90,13 +95,20 @@ var GistEditor = React.createClass({
     var updatedGist = this.editedGist
     if(updatedGist)
       gist.save(this.props.gistId, JSON.stringify({files:updatedGist.toJS()}))
+          .then(()=>{
+      setTimeout(this.refs.snackbar.dismiss,2000)
+      this.refs.snackbar.show()
+          })
+          .catch(()=>{
+      setTimeout(this.refs.snackbar.dismiss,2000)
+      this.refs.save_fail.show()
+          })
   },
   _togglePreview: function(){
     this.setState({preview:!this.state.preview},()=>{
       if(this.state.preview){
-        when.all(this.codemirrors.map((codemirror)=>{
-          return markdown(JSON.stringify({text:codemirror.getValue()}))
-        }).toArray()).then((data)=>{
+        when.all(markdown(JSON.stringify({text:this.editedGist})))
+            .then((data)=>{
           this.setState({markdowns:im.List(data)})
         })
       }
