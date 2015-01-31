dbf3f4336bb2417a5a03ee7f148368b73bfdb1c8
Jichao Ouyang <oyanglulu@gmail.com>
2015-01-31 22:46:49 +0800

    get hostname from user info

--- stores/client.js
+++ stores/client.js
@@ -2,9 +2,13 @@ var rest = require('rest'),
     pathPrefix = require('rest/interceptor/pathPrefix'),
     errorCode = require('rest/interceptor/errorCode'),
     mime = require('rest/interceptor/mime'),
-    defaultRequest = require('rest/interceptor/defaultRequest')
+    defaultRequest = require('rest/interceptor/defaultRequest');
 var api_remote = 'http://private-4d99c-igist.apiary-mock.com';
-if (process.env.NODE_ENV === "production") api_remote = 'https://api.github.com';
+var endpoint = 'https://api.github.com';
+if(localStorage.currentuser)
+  endpoint = new URL(JSON.parse(localStorage.currentuser).url).hostname || endpoint;
+
+if (process.env.NODE_ENV === "production") api_remote = endpoint;
 var client =
       rest.wrap(mime)
       .wrap(errorCode, { code: 500 })
