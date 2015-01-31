aac88bf9fac0f6eb6ed4a9b75503f0afd3c61245
Jichao Ouyang <oyanglulu@gmail.com>
2015-01-31 23:09:02 +0800

    login with enterprise

--- components/login.jsx
+++ components/login.jsx
@@ -1,7 +1,7 @@
 var React = require('react'),
 $E = require('./event'),
 mui = require('material-ui'),
-{Dialog, RaisedButton} = mui,
+{Input, Dialog, RaisedButton} = mui,
 auth = require('../stores/authenticate'),
 Stage = require('./stage');
 var LoginDialog = React.createClass({
@@ -25,18 +25,26 @@ var LoginDialog = React.createClass({
     return {
       id: '',
       title: "!gist",
-      actions: []
+      actions: [],
+      enterprise: false
     }
   },
+  
   render: function(){
     return (
     	  <div className="home-page-hero full-width-section">
           <div className="tagline">
-            <h1 className="brand-name">!gist != gist</h1>
-            <h2 className="brand-name">it's for anything you wanna keep</h2>   
-            <div className="button-group">
+            <h1 className="brand-name">!gist</h1>
+            <h2 className="brand-name">is for anything you wanna keep</h2>
+
+            <div className="form-group">
+              <div className={"enterprise" + (this.state.enterprise?'':' hidden')}>
+                <Input type="input" name="username" className={this.enterprise} placeholder="username"/>
+                <Input type="password" name="password" className={this.enterprise} placeholder="password"/>
+                <Input type="url" name="url" className={this.enterprise} placeholder="https://your.github.com/api/v3"/>
+              </div>
             <RaisedButton label="Login GitHub" onTouchTap={this._onRedirectToAuth} />
-            <RaisedButton label="Login GitHub Enterpise" />
+            <RaisedButton label="Login GitHub Enterpise" onTouchTap={this._handleEnterpriseLogin}/>
             </div>
           </div>
           
@@ -46,21 +54,19 @@ var LoginDialog = React.createClass({
   componentDidMount: function(){
     $E.on('dialog.showConfirm', this._onShowConfirm)
   },
-  _onRedirectToAbout: function(){
-    window.location.href = "/about";
-  },
   _onConfirm: function(){
     $E.trigger('dialog.confirm', true)
-  },
-  _onShowConfirm: function(data){
-    this.setState({actions:this.actions().confirm, title: data.title, id:data.id});
-    this.refs.dialog.show();
   },
   _onRedirectToAuth: function(){
     if (process.env.NODE_ENV == "production")
       window.location.href = "https://github.com/login/oauth/authorize?client_id=f595ed9c6c53f4cb4257&scope=gist"
     else
       window.location.href = "/?code=AUTH_DONE_DA"
+  },
+  _handleEnterpriseLogin: function(){
+    this.setState({
+      enterprise: true
+    })
   }
 })
 
