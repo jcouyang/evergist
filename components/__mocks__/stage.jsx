19120f3dd246262dedc1392490cc587d98456f26
Jichao Ouyang <oyanglulu@gmail.com>
2015-02-19 22:53:15 +0800

    rm console.log

--- components/__mocks__/stage.jsx
+++ components/__mocks__/stage.jsx
@@ -1,7 +1,6 @@
 var React = require('react');
 var Stage = React.createClass({
   render: function(){
-    console.log(this.props.children,'----------------')
     return (
       <div className="stage">
         {this.props.children}
