4a584d83349774ca6761190202c33a555bd18184
Jichao Ouyang <oyanglulu@gmail.com>
2015-02-21 23:48:43 +0800

    edit button

--- components/gist-digest.jsx
+++ components/gist-digest.jsx
@@ -1,14 +1,24 @@
-var React = require('react');
-var {FloatingActionButton} = require('material-ui');
+var React = require('react/addons');
+var cx = React.addons.classSet;
+var {IconButton} = require('material-ui');
 var moment = require('moment');
 module.exports = React.createClass({
   render: function(){
+    var iconClasses = cx({
+      'fa fa-eye': this.props.edit,
+      'fa fa-pencil': !this.props.edit
+    })
+    var classes = cx({
+      'hidden': !this.props.display
+    })
     return (
       <a href={'#'+get(this.props.gist,'id')} className="gist-item">
-        <FloatingActionButton icon={this.props.edit?'image-remove-red-eye':'editor-mode-edit'}
-                              onTouchTap={this.props.onEdit}
-                              className={this.props.className + " action-button edit"}
-                              mini={true}/>
+        <div className="right">
+          <IconButton iconClassName={iconClasses}
+                      className={classes}
+                      onTouchTap={this.props.onEdit}/>
+        </div>
+
         <div className="gist-digest" onClick={this.props.onClick}>
             <time className="mui-font-style-caption">
               {moment(get(this.props.gist,'updated_at')).fromNow()}
